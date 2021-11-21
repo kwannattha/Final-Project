33ed2ff9bc80748162e80766b22de070ff775597
kwannattha <58881048+kwannattha@users.noreply.github.com>
2021-11-22 00:13:38 +0700

    final

---
+++ Lms/Source/librarian/contactus.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Library Management System</title>
+	<link rel="icon" type="image/png" href="dist/img/mm.png">
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="dist/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="dist/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="dist/css/animate.css">
+	<link rel="stylesheet" href="dist/css/main.css">
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+					<div class="logo">
+						<img src="dist/img/1.png" alt="logo">
+					</div>
+				</div>
+				<div class="col-9">
+					<div class="header-right">
+						<ul>
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+							<li><a href="index.php">Home</a></li>
+						</ul>		
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contactinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="left">
+						<h3>Send Us a Message</h3>
+						<form action="">
+							<input type="text" placeholder="Your Name">
+							<input type="email" placeholder="Your E-mail">
+							<input type="digit" placeholder="Phone Number">
+							<textarea placeholder="Your Message" id="" cols="60" rows="5"></textarea>
+							<input type="submit" value="SEND A MESSAGE">
+						</form>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="right">
+						<h3>Contact Info</h3>
+
+						<div class="box">
+							<p><i class="fas fa-home"></i><span>Khon Kaen University Nong Khai Campus, Nong Khai</span></p>
+							<p><i class="far fa-envelope"></i><span>nattha.sonsri@kkumail.com</span></p>
+							<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;netnapha_th@kkumail.com</p>
+							<p><i class="fas fa-phone"></i><span>(+66)2 254 XXXX</span></p>
+							<p> (+66)2 254 XXXX</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="dist/js/jquery-2.2.4.min.js"></script>
+	<script src="dist/js/bootstrap.min.js"></script>
+	<script src="dist/js/owl.carousel.min.js"></script>
+	<script src="dist/js/owl.animate.js"></script>
+	<script src="dist/js/superfish.js"></script>
+	<script src="dist/js/waypoints.min.js"></script>
+	<script src="dist/js/jquery.counterup.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+</body>
+</html>
